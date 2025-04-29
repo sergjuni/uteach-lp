44dac206c0baddb825f8e7054dfe921ccaf260e7
sergio <serrgio.madureira@gmail.com>
2025-04-29 16:15:20 -0300

    removes the react-helmet package because of conflicts with the react version, done the SEO optmization through the index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { HelmetProvider, Helmet } from 'react-helmet-async';
 import Header from './components/Header'
 import Hero from './sections/Hero'
 import MainTestimonials from './sections/MainTestimonials'
@@ -11,31 +10,19 @@ import Form from './sections/Form'
 
 function App() {
   return (
-    <HelmetProvider>
-      <div className="App">
-        <Helmet>
-          <title>UTeach - Connect with International Students & Teachers</title>
-          <meta name="description" content="Join UTeach to connect with students and teachers worldwide. Access quality education, interactive learning tools, and a global community of learners." />
-          <meta name="keywords" content="online education, teaching platform, international students" />
-          <meta property="og:title" content="UTeach - Global Education Platform" />
-          <meta property="og:description" content="Connect with students and teachers worldwide" />
-        </Helmet>
-
-        <div className="app">
-          <Header />
-          <main>
-            <Hero />
-            <MainTestimonials />
-            <MeetCommunity />
-            <UserTestimonials />
-            <Feature />
-            <Learning />
-            <Form />
-          </main>
-          <Footer />
-        </div>
+      <div className="app">
+        <Header />
+        <main>
+          <Hero />
+          <MainTestimonials />
+          <MeetCommunity />
+          <UserTestimonials />
+          <Feature />
+          <Learning />
+          <Form />
+        </main>
+        <Footer />
       </div>
-    </HelmetProvider>
   )
 }
 
